919f2fbe985cf270defe55a6f0ae864c4b645f7a
tyomkin1 <tyomkina@digicode.net>
2018-08-09 15:38:16 +0300

    init

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <div className="notice">Best site for Digicode!</div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
